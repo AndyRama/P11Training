5cc444d75da8cd79bcd213ec3fb0910739b57468
Andy Ramaroson <andyramaroson@gmail.com>
2022-10-11 18:51:24 +0200

    [enhancement] add style to Navbar.scss

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Navbar.scss"
 import Logo from './../../assets/images/Logo.png'
+import {  Link , useLocation } from'react-router-dom'
 
 function Navbar() {
+
+  const location = useLocation()
+
   return(
 
     <header className="header">
       <nav className="Navbar">
-        <img src={Logo} alt="Logo entreprise kasa" />
+        <img src={Logo} alt="Logo entreprise kasa"/>
+
+        <div className="Nav-Btn-Wrapper">
+          <Link to="/" className={ location.page === '/' ? 'Home-page active' : 'Home-page'} page="home">Acceuil</Link>
+          <Link to="/About" className={ location.page === '/About' ? 'About-page active' : 'About-page'} page="About">A Propos</Link>
+        </div>
       </nav>
     </header>
-
   )
 }
 
