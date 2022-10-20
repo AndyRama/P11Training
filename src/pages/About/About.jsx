f249e2b3345426bb4bdb6440cd513ad2cecb16db
Andy Ramaroson <andyramaroson@gmail.com>
2022-10-20 22:58:08 +0200

    [fix]add text content for collapse page about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,25 +6,22 @@ import ImageBanner from "./../../assets/images/BannerAbout.png"
 function About() {
   const collapse = [
     {
-      title:"fiabilité",
-      text: "test"
+      title: "Fiabilité",
+      text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
     },
-
     {
-      title:"fiabilité",
-      text: "test"
+      title: "Respect",
+      text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
     },
-
     {
-      title:"fiabilité",
-      text: "test"
+      title: "Service",
+      text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
     },
-
     {
-      title:"fiabilité",
-      text: "test"
-    }
-  ]
+      title: "Sécurité",
+      text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
 
   return (
     <section>
